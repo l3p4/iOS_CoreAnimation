9a23037d9004849ec283b00c09b2d47446de2382
Simon <sirlin12@163.com>
2024-01-20 16:45:40 +0800

    1.Create project

---
+++ iOS_CoreAnimation_Demo/iOS_CoreAnimation_Demo/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  iOS_CoreAnimation_Demo
+//
+//  Created by simon on 2024/1/20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
