4a363b658f2a1527ba76f0a7c05290164e5bbffc
Simon <sirlin12@163.com>
2024-02-26 21:58:49 +0800

    no message

---
+++ iOS_CoreAnimation_Demo/iOS_CoreAnimation_Demo/Tools/UIColor+random.swift
@@ -0,0 +1,18 @@
+//
+//  UIColor+random.swift
+//  iOS_CoreAnimation_Demo
+//
+//  Created by simon on 2024/2/26.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static var random: UIColor {
+        return UIColor(red: .random(in: 0...1),
+                       green: .random(in: 0...1),
+                       blue: .random(in: 0...1),
+                       alpha: 1.0)
+    }
+}
